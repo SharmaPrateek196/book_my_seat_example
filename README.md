ad13d2f05aeaa1489b18ab4e58d81fbb430b0e79
Prateek Sharma <sharmaprateek196@gmail.com>
2023-02-05 00:55:16 +0530

    initial & final commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# bus_seat_booking_example
+
+This is an example project which explains the use of a flutter package [book_my_seat](https://pub.dev/packages/book_my_seat) which helps you create seat booking layout for bus, theatre, aeroplane etc.
+
+## Blog explanation
+Read this [medium blog](https://medium.com/@sharmaprateek196/how-to-create-seat-booking-layout-in-flutter-33cff82b3edc) for understanding it more better.
+
+## Final output
+
+<img src="https://user-images.githubusercontent.com/47804278/216785516-482f6888-ce6e-46eb-a512-7bd53308072f.jpg" alt="bus layout" width="250" height="480">
---
+++ lib/main.dart
@@ -0,0 +1,259 @@
+import 'package:book_my_seat/book_my_seat.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'book_my_seat package example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const BusLayout(),
+    );
+  }
+}
+
+class BusLayout extends StatefulWidget {
+  const BusLayout({Key? key}) : super(key: key);
+
+  @override
+  State<BusLayout> createState() => _BusLayoutState();
+}
+
+class _BusLayoutState extends State<BusLayout> {
+  Set<SeatNumber> selectedSeats = {};
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 16,
+            ),
+            const Text("Front of bus is this side"),
+            const SizedBox(
+              height: 32,
+            ),
+            Flexible(
+              child: SizedBox(
+                width: double.maxFinite,
+                height: 500,
+                child: SeatLayoutWidget(
+                  onSeatStateChanged: (rowI, colI, seatState) {
+                    if (seatState == SeatState.selected) {
+                      selectedSeats.add(SeatNumber(rowI: rowI, colI: colI));
+                    } else {
+                      selectedSeats.remove(SeatNumber(rowI: rowI, colI: colI));
+                    }
+                  },
+                  stateModel: const SeatLayoutStateModel(
+                    rows: 10,
+                    cols: 7,
+                    seatSvgSize: 45,
+                    pathSelectedSeat: 'assets/seat_selected.svg',
+                    pathDisabledSeat: 'assets/seat_disabled.svg',
+                    pathSoldSeat: 'assets/seat_sold.svg',
+                    pathUnSelectedSeat: 'assets/seat_unselected.svg',
+                    currentSeatsState: [
+                      [
+                        SeatState.disabled,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.empty,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.sold,
+                      ],
+                      [
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.empty,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                      ],
+                      [
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.empty,
+                        SeatState.sold,
+                        SeatState.sold,
+                        SeatState.sold,
+                      ],
+                      [
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.empty,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                      ],
+                      [
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.empty,
+                        SeatState.unselected,
+                        SeatState.sold,
+                        SeatState.sold,
+                      ],
+                      [
+                        SeatState.sold,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.empty,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                      ],
+                      [
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.empty,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                      ],
+                      [
+                        SeatState.sold,
+                        SeatState.sold,
+                        SeatState.unselected,
+                        SeatState.empty,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                      ],
+                      [
+                        SeatState.empty,
+                        SeatState.empty,
+                        SeatState.empty,
+                        SeatState.empty,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.sold,
+                      ],
+                      [
+                        SeatState.unselected,
+                        SeatState.unselected,
+                        SeatState.sold,
+                        SeatState.sold,
+                        SeatState.sold,
+                        SeatState.unselected,
+                        SeatState.unselected,
+                      ],
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 15,
+                        height: 15,
+                        color: Colors.grey.shade700,
+                      ),
+                      const SizedBox(width: 2),
+                      const Text('Disabled')
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 15,
+                        height: 15,
+                        color: Colors.lightBlueAccent,
+                      ),
+                      const SizedBox(width: 2),
+                      const Text('Sold')
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(border: Border.all(color: const Color(0xff0FFF50))),
+                      ),
+                      const SizedBox(width: 2),
+                      const Text('Available')
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 15,
+                        height: 15,
+                        color: const Color(0xff0FFF50),
+                      ),
+                      const SizedBox(width: 2),
+                      const Text('Selected by you')
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith((states) => const Color(0xFFfc4c4e)),
+              ),
+              child: const Text('Show my selected seat numbers'),
+            ),
+            const SizedBox(height: 12),
+            Text(selectedSeats.join(" , "))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SeatNumber {
+  final int rowI;
+  final int colI;
+
+  const SeatNumber({required this.rowI, required this.colI});
+
+  @override
+  bool operator ==(Object other) {
+    return rowI == (other as SeatNumber).rowI && colI == other.colI;
+  }
+
+  @override
+  int get hashCode => rowI.hashCode;
+
+  @override
+  String toString() {
+    return '[$rowI][$colI]';
+  }
+}
